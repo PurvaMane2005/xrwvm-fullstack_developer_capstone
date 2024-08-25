5e1522f53b452ca5d64c2b860a2bb25908ea5fa9
PurvaMane2005 <purvamane2005@gmail.com>
2024-08-25 10:47:59 -0400

    commit 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,12 +9,15 @@ urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout',view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
     path('get_dealers', views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
+
 
     # path for dealer reviews view
 
